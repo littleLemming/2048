4c8bd98abef992f39843dafe639e5f3b55242009
littleLemming <lemming0061@gmail.com>
2016-02-24 19:53:16 +0100

    ze comments

--- 2048.py
+++ 2048.py
@@ -3,44 +3,56 @@ import pprint
 
 class Field():
 
+	# constructor
+	# size: should be lager than 2 - sets the length and width of the field
 	def __init__(self,size):
 		self.field_size = size if size > 2 else 4
 		self.field = [[0]*self.field_size for i in range(self.field_size)]
 
+	# returns the multidimensional array that represents the field
+	# should not be used outside of testing
 	def get_field(self):
 		return self.field
 
+	# returns an array of the row with the row_num
 	def get_row(self,row_num):
-		if row_num < self.field_size:
+		if row_num < self.field_size and row_num >= 0:
 			return self.field[row_num]
 		return None
 
+	# returns an array of the column with the column_num
 	def get_column(self,column_num):
-		if column_num < self.field_size:
+		if column_num < self.field_size and column_num >= 0:
 			column = [0]*self.field_size
 			for i in range(self.field_size):
 				column[i] = self.field[i][column_num]
 			return column
 		return None
 
+	#returns the value of the spot at row/column
 	def get_value(self,row,column):
 		if row < self.field_size and column < self.field_size:
 			return self.field[row][column]
 		return None
 
-	# should be just used for testing and maybe from inside Field...dunno yet
+	# sets the spcified spot at row/column to value
+	# should not be used from anywhere except from inside the class 'Field' or for testing
 	def set_value(self,row,column,value):
-		if row < self.field_size and column < self.field_size:
+		if row < self.field_size and and row >= 0 column < self.field_size and column >= 0:
 			self.field[row][column] = value
 			return True
 		return False
 
+	# returns True, if the specified row is already full, else False
+	# 0 <= row < field_size
 	def is_full_row(self,row):
 		for i in range(self.field_size):
 			if self.field[row][i] == 0:
 				return False
 		return True
 
+	# returns the amount of empty fields in row
+	# 0 <= row < field_size
 	def count_empty_row(self,row):
 		count = 0
 		for i in range(self.field_size):
@@ -48,12 +60,16 @@ class Field():
 				count += 1
 		return count
 
+	# returns True, if the specified column is already full, else False
+	# 0 <= column < field_size
 	def is_full_column(self,column):
 		for i in range(self.field_size):
 			if self.field[i][column] == 0:
 				return False
 		return True
 
+	# returns the amount of empty fields in column
+	# 0 <= column < field_size
 	def count_empty_column(self,column):
 		count = 0
 		for i in range(self.field_size):
@@ -61,15 +77,18 @@ class Field():
 				count += 1
 		return count
 	
+	# returns if the spot row/column is already full
 	def is_full_spot(self,row,column):
 		return not self.field[row][column] == 0
 
+	# returns if there is any empty space
 	def is_full_field(self):
 		for i in range(self.field_size):
 			if not self.is_full_row(i):
 				return False
 		return True
 
+	# adds in any empty place of the field either a 2 or a 4
 	def add_random_number(self):
 		if not self.is_full_field():
 			row = random.randint(0,self.field_size-1)
