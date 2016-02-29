b8db6c5dbbbcd45e70df6421c99a1f4b455661c4
littleLemming <lemming0061@gmail.com>
2016-02-29 12:15:51 +0100

    removed duplicate code

--- 2048.py
+++ 2048.py
@@ -146,32 +146,7 @@ class Field():
 					self.set_value(i,j,new_row[j])
 				else:
 					self.set_value(j,i,new_row[j])
-				
-
-
-	def push_right(self):
-		for i in range(0,self.field_size-1):
-			new_row = self.resort_array(self.get_row(i),'to_end')
-			for j in range(0,self.field_size-1):
-				self.set_value(i,j,new_row[j])
-
-	def push_left(self):
-		for i in range(0,self.field_size-1):
-			new_row = self.resort_array(self.get_row(i),'to_start')
-			for j in range(0,self.field_size-1):
-				self.set_value(i,j,new_row[j])
-
-	def push_up(self):
-		for i in range(0,self.field_size-1):
-			new_column = self.resort_array(self.get_column(i)),'to_start')
-			for j in range(0,self.field_size-1):
-				self.set_value(j,i,new_row[j])
-
-	def push_down(self):
-		for i in range(0,self.field_size-1):
-			new_column = self.resort_array(self.get_column(i)),'to_end')
-			for j in range(0,self.field_size-1):
-				self.set_value(j,i,new_row[j])
+					
 
 class Game():
 
