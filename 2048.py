277b4a13e7296b8647cd7e51eb2da6374c773c10
littleLemming <lemming0061@gmail.com>
2017-07-21 23:39:03 +0200

    few minor changes to easier build a game

--- 2048.py
+++ 2048.py
@@ -11,6 +11,7 @@ class Field():
 		self.add_random_number()
 		self.add_random_number()
 		self.game_over = False
+		self.max_num = 0
 
 	# returns the multidimensional array that represents the field
 	def get_field(self):
@@ -158,6 +159,8 @@ class Field():
 					if last != -1:
 						if x == last:
 							new_line[spot] = x**2
+							if new_line[spot] > self.max_num:
+								self.max_num = new_line[spot]
 							last = -1
 							spot += 1
 						else:
@@ -182,50 +185,9 @@ class Field():
 
 
 
-class Game():
+#class Game():
 
-	# tupel - x,y,value
-
-	def __init__(self,size):
-		field = Field(size)
-		for i in [1,2]:
-			if not self.add_number():
-				print('cannot add {0}'.format(i))
-				raise StandardError
-
-	# to be called after the field has been pushed or twice in the very beginning
-	# returns True if a number has been added, if not there is no space left - lost
-	# TODO check if this method covers all lost cases
-	def add_number(self):
-		return True
-
-	def sort(self,direction, array):
-		return
-		#TODO
-
-	# puts arrays into threads to be sorted
-	# direction can be 'u', 'd', 'l', 'r'
-	def split(self,direction):
-		if direction == 'u' or direction == 'd':
-			#spalten
-			array = sort(direction,array) #in thread
-
-		elif direction == 'l' or direction == 'r':
-			#zeilen
-			array = sort(direction,array) #in thread
-
-
-	def move_up(self):
-		split('u')
-
-	def move_down(self):
-		spilt('d')
-
-	def move_left(self):
-		split('l')
-
-	def move_right(self):
-		split('r')
+	
 
 ###############TESTING###############
 
